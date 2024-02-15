18c9fe8803f11075be7eaf3c45cf44c6d272f136
Akila Mendis <akila@oneit.com.au>
2024-02-15 17:01:36 +0530

    150. Create new Maven Project and all Framework dependencies

--- src/test/java/akila/SeleniumFrameworkDesign/AppTest.java
+++ src/test/java/akila/SeleniumFrameworkDesign/AppTest.java
@@ -1,13 +1,11 @@
 package akila.SeleniumFrameworkDesign;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
     
-  @Test
+
   public void shouldAnswerWithTrue() {
-    assertTrue(true);
+
   }
 }

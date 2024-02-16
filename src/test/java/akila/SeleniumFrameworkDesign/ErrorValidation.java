bb6a0c7ccac50a3f81666ea3031dc1dba0e9529f
Akila Mendis <akila@oneit.com.au>
2024-02-16 14:23:24 +0530

    168. Implement Test Strategy for Framework on how tests are divided based on modules

--- src/test/java/akila/SeleniumFrameworkDesign/ErrorValidation.java
+++ src/test/java/akila/SeleniumFrameworkDesign/ErrorValidation.java
@@ -6,18 +6,30 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import akila.TestComponents.BaseTest;
+import pageobjects.CartPage;
 import pageobjects.ProductCatalog;
 
 public class ErrorValidation extends BaseTest {
 	String username = "anshika@gmail.com";
-	String password = "Iamking@0001";
+	String password = "Iamking@000";
 	String productName = "ZARA COAT 3";
 	String countryName = "india";
 	
 	@Test
-	public void submitOrder() throws IOException, InterruptedException {
+	public void loginErrorValidation() throws IOException, InterruptedException {
+		
+		landingPage.loginApplication(username, password+"1");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());;
+	}
+	
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException {
 		
 		ProductCatalog productCatalogPage = landingPage.loginApplication(username, password);
-		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());;
+		productCatalogPage.addProductToCart(productName);
+		CartPage cartPage = productCatalogPage.goToCartPage();
+		
+		Boolean match = cartPage.verifyProductDisplay("ZARA COAT 33");
+		Assert.assertTrue(match);
 	}
 }
